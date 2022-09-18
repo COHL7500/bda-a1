f22baee228b2a9baea6b97eb89b81b74357b26bc
COHL7500 <cohl7500@gmail.com>
2022-09-18 15:25:23 +0200

    fix readability of code

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 // inspiration: https://gist.github.com/nmjmdr/d3637b726b564033d318
 var wg sync.WaitGroup
+
 var free = make([]chan bool, 5)
 var done = make([]chan bool, 5)
 
@@ -22,53 +23,61 @@ func fork(i int) {
 	}
 }
 
-func phil(i int) {
-	var e = 0
-	fmt.Println(i, " started eating")
+func philosopher(i int) {
+	var rightFork = (i + 1) % 5
+	var leftFork = i
+	var biteCount = 0
+	fmt.Println("Philosopher", i, ": started eating...")
 
 	for {
-		left := false
-		right := false
+		isLeftFree := false
+		isRightFree := false
 
 		select {
-		case left = <-free[i]:
+
+		case isLeftFree = <-free[leftFork]:
 			break
+
 		default:
-			left = false
+			isLeftFree = false
 			break
 		}
 
 		select {
-		case right = <-free[(i+1)%5]:
+
+		case isRightFree = <-free[rightFork]:
 			break
+
 		default:
-			right = false
+			isRightFree = false
 			break
 		}
 
-		if left && !right {
-			done[i] <- true
-		} else if !left && right {
-			done[(i+1)%5] <- true
-		} else if left && right {
-			fmt.Println(i, " eating", e+1, "/3")
+		if isLeftFree && !isRightFree {
+			done[leftFork] <- true
+
+		} else if !isLeftFree && isRightFree {
+			done[rightFork] <- true
+
+		} else if isLeftFree && isRightFree {
+			fmt.Println("Philosopher", i, ": Eating", biteCount+1, "/ 3")
 			time.Sleep(1000 * time.Millisecond)
-			done[i] <- true
-			done[(i+1)%5] <- true
-			e = e + 1
-			fmt.Println(i, " thinking")
-			if e == 3 {
+			done[leftFork] <- true
+			done[rightFork] <- true
+			biteCount = biteCount + 1
+			fmt.Println("Philosopher", i, ": Thinking...")
+			if biteCount == 3 {
 				break
 			}
 		}
 	}
 
-	fmt.Println(i, " finished eating")
+	fmt.Println("Philosopher", i, ": Finished eating!")
 	wg.Done()
 }
 
 func main() {
-	fmt.Println("empty table")
+	fmt.Println("Eating commences...")
 	wg.Add(5)
 
 	for i := 0; i < 5; i++ {
@@ -78,9 +87,9 @@ func main() {
 
 	for i := 0; i < 5; i++ {
 		go fork(i)
-		go phil(i)
+		go philosopher(i)
 	}
 
 	wg.Wait()
-	fmt.Println("empty table")
+	fmt.Println("------------\nFINISHED: All philosophers have taken 3 bites.")
 }
